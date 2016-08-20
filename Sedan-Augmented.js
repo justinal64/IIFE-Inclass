9f651c12d5045b252db75659cc871068f298733b
justin leggett <justin@codingrookie.com>
2016-08-20 10:09:53 -0500

    added Sedan.aug and Sdean.js

---
+++ Sedan-Augmented.js
@@ -0,0 +1,19 @@
+var Sedan = (function(newSedan) {
+    var max_occupancy = 6;
+    var current_occupancy = 0;
+
+    newSedan.getOccupancy = function() {
+        return current_occupancy;
+    };
+
+    newSedan.setOccupancy = function(occupancy) {
+        if(occupancy <= max_occupancy){
+            current_occupancy = occupancy;
+        } else {
+            throw "Cannot exceed maximum occupancy of " + max_occupancy;
+        }
+    };
+
+    return newSedan;
+
+})(Sedan || {});
--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("main.js");
+
+console.log("Sedan IIFE", Sedan);
+console.log("Sedan color", Sedan.getColor());
+console.log("Sedan type", Sedan.getType());
+console.log("Sedan set Occupancy", Sedan.setOccupancy(9));
+console.log("Sedan get Occupancy", Sedan.getOccupancy());
+
